5f8251d3c2f4856f3ac2906fde495fd60d42bfdf
bayzhen <1521085156@qq.com>
2022-12-12 21:10:22 +0800

    markov

--- RL/ChatGPT/08.py
+++ RL/ChatGPT/08.py
@@ -1,6 +1,8 @@
+import gym
 import numpy as np
 from typing import Any, Dict, Generator, List, Optional, Union
 import matplotlib.pyplot as plt
+import torch.cuda
 from stable_baselines3 import DQN, PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env.vec_normalize import VecNormalize
@@ -10,65 +12,68 @@ from scipy.stats import norm
 from stable_baselines3.common.type_aliases import (
     ReplayBufferSamples
 )
+from gym import spaces
+import torch as th
+from env_simulator import EnvSimulator
 
 
 class PriorityReplayBuffer(ReplayBuffer):
-    def __init__(self, size, observation_space, action_space, device, n_envs, optimize_memory_usage):
-        # Call the __init__() method of the parent class
-        super(PriorityReplayBuffer, self).__init__(size,
-                                                   observation_space=observation_space,
-                                                   action_space=action_space,
-                                                   device=device,
-                                                   n_envs=n_envs,
-                                                   optimize_memory_usage=optimize_memory_usage,
-                                                   )
-        self.trajectory = []
+    def __init__(
+            self,
+            buffer_size: int,
+            observation_space: spaces.Space,
+            action_space: spaces.Space,
+            device: Union[th.device, str] = "cpu",
+            n_envs: int = 1,
+            optimize_memory_usage: bool = False,
+            handle_timeout_termination: bool = True,
+    ):
+        super(PriorityReplayBuffer, self).__init__(buffer_size,
+                                                   observation_space,
+                                                   action_space,
+                                                   device,
+                                                   n_envs,
+                                                   optimize_memory_usage,
+                                                   handle_timeout_termination)
+        if type(observation_space) is gym.spaces.box.Box:
+            v0 = 1
+            for i in observation_space.shape:
+                v0 *= i
+            state_input_size = v0
+        if type(action_space) is gym.spaces.discrete.Discrete:
+            action_input_size = 1
+        self.env_simulator = EnvSimulator(state_input_size, action_input_size).to(self.device)
+        self.optimizer = torch.optim.Adam(self.env_simulator.parameters(), lr=0.001)
 
-    def add(self, obs, next_obs, action, reward, done, infos):
-        # Call the add() method of the parent class
-        self.trajectory.append([obs, next_obs, action, reward, done, infos])
-        if done.item() is True:
-            if len(self.trajectory) > 100:
-                train_list = self.trajectory[int(len(self.trajectory) / 2):]
-            else:
-                train_list = self.trajectory
-            for step in train_list:
-                obs = step[0]
-                next_obs = step[1]
-                action = step[2]
-                reward = step[3]
-                done = step[4]
-                infos = step[5]
-                super(PriorityReplayBuffer, self).add(obs, next_obs, action, reward, done, infos)
-            self.trajectory.clear()
-
-    def my_sample(self, batch_size: int, env: Optional[VecNormalize] = None):
-        # Calculate the mean and standard deviation of the rewards
-        super(PriorityReplayBuffer, self).sample()
-        rewards = self.rewards
-        mean = np.mean(rewards)
-        std = np.std(rewards)
-
-        x = np.linspace(-5, 5, 100)
-        y = norm.pdf(x, mean, std)
-        plt.plot(x, y)
-        plt.show()
-
-        # Calculate the probability of each reward using the normal distribution
-        probabilities = norm.pdf(rewards, mean, std)
-        probabilities = np.array(probabilities).flatten()
-        probabilities = 1 / probabilities
-        probabilities = probabilities / sum(probabilities)
-        indexes = np.random.choice(len(rewards), p=probabilities, size=(batch_size))
-        return self._get_samples(indexes, env=env)
+    def sample(self, batch_size: int, env: Optional[VecNormalize] = None) -> ReplayBufferSamples:
+        rewards = np.expand_dims(self.rewards, axis=2)
+        if self.full:
+            nn_input = np.concatenate((self.observations, self.actions), axis=2)
+            nn_output = np.concatenate((rewards, self.next_observations), axis=2)
+        else:
+            nn_input = np.concatenate((self.observations, self.actions), axis=2)[:self.pos]
+            nn_output = np.concatenate((rewards, self.next_observations), axis=2)[:self.pos]
+        nn_input = nn_input.astype(np.float32)
+        nn_output = nn_output.astype(np.float32)
+        nn_input = torch.tensor(nn_input).to(self.device)
+        nn_output = torch.tensor(nn_output).to(self.device)
+        criterion = torch.nn.MSELoss()
+        loss = criterion(self.env_simulator(nn_input), nn_output)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        loss = loss.item()
+        print(f"loss: {loss:>7f}")
+        return super(PriorityReplayBuffer, self).sample(batch_size, env)
 
 
 # Create the CartPole environment using the make_vec_env function from stable-baselines
-game_env = make_vec_env('LunarLander-v2', n_envs=1)
+game_env = make_vec_env('CartPole-v1', n_envs=1)
 
 # Create the DQN model using a CNN policy and the PriorityReplayBuffer instance
 model = DQN("MlpPolicy",
             env=game_env,
+            replay_buffer_class=PriorityReplayBuffer,
             verbose=1,
             tensorboard_log='./DQN/')
 
